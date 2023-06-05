0c9b756b997a820489828e0c401b123713632756
vladankd <vladankd@gmail.com>
2023-06-05 22:55:41 +0200

    Add reset colors button

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,15 +2,32 @@ let containerWidth = 720;
 
 const container = document.querySelector(".container");
 
-function createGrid(grid) {
-  console.log(grid);
-  let size = containerWidth / grid;
-  for (let i = 0; i < grid; i++) {
+createGrid(32);
+
+const pixels = document.querySelectorAll(".pixel");
+
+const resetGrid = document.querySelector("#reset_btn");
+resetGrid.addEventListener("click", clearGridColors);
+
+pixels.forEach((px) => {
+  px.addEventListener("mouseover", (e) => {
+    let color = "rgb(123,123,123)";
+    if (e.buttons) {
+      e.target.style.backgroundColor = color;
+    }
+  });
+});
+
+// progaram functions
+
+function createGrid(gridSize) {
+  let size = containerWidth / gridSize;
+  for (let i = 0; i < gridSize; i++) {
     const row = document.createElement("div");
 
     row.classList.add("row");
     container.appendChild(row);
-    for (let j = 0; j < grid; j++) {
+    for (let j = 0; j < gridSize; j++) {
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
       pixel.style.height = `${size}px`;
@@ -20,4 +37,8 @@ function createGrid(grid) {
   }
 }
 
-createGrid(64);
+function clearGridColors() {
+  pixels.forEach((px) => {
+    px.style.backgroundColor = "white";
+  });
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 #Etch-a-Sketch application
 
 - create 16x16 grid of divs in javascript
+- create reset grid colors functionality
